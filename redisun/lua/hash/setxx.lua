450a4139a5162df5de67def79095a28d2394648d
李孟响 <limengxiang876@gmail.com>
2018-09-13 16:52:22 +0800

    fix hash setxx & hash setnx

--- redisun/lua/hash/setxx.lua
+++ redisun/lua/hash/setxx.lua
@@ -2,20 +2,24 @@ local vs={}
 local lt=tonumber(ARGV[2])
 local ex=ARGV[1]
 for i,k in ipairs(KEYS) do
-  if redis.call('EXISTS',k) == 1 then
+  local st=false
+  local tp=redis.call('TYPE',k)['ok']
+  if tp=='hash' then
     local ms=redis.call('HMSET',k,_SET_KVS_)['ok']
-    if lt > 0 and ms == 'OK' then
-      if ex == 'EX' then
-        redis.call('EXPIRE',k,lt)
-      else
-        redis.call('PEXPIRE',k,lt)
+    if ms=='OK' then
+      st=0
+      if lt>0 then
+        if ex == 'EX' then
+          redis.call('EXPIRE',k,lt)
+        else
+          redis.call('PEXPIRE',k,lt)
+        end
       end
-      vs[i]={ k,0,ms }
     else
-      vs[i]={ k,2,ms }
+      st=2
     end
   else
-    vs[i]={ k,1,false }
+    st=3
   end
 end
 return vs
